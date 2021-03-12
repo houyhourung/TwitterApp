75543f739d60b21fb374d7bb5bc8d661637419a6
houyhourung <houyhourung88@gmail.com>
2021-03-12 11:46:57 -0500

    User can now tweet

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class TweetCellTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
